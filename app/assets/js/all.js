f8249ae3f250e517221aee4bc4e29427e6c89847
wendy03 <wendysu03@gmail.com>
2020-12-20 11:43:52 +0800

    新增: Swiper JS & Datepicker JS

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,45 @@
-console.log('Hello!');
-
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  $("#dropdownDate").daterangepicker();
+});
+
+var swiper = new Swiper(".swiper-top", {
+	slidesPerView: 1.3,
+	spaceBetween: 30,
+	loop: true,
+	breakpoints: {
+		767: {
+			slidesPerView: 2
+		},
+		991: {
+			slidesPerView: 3
+		},
+		1200: {
+			slidesPerView: 4
+		}
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	}
+});
+var swiper = new Swiper(".swiper-inspiration", {
+	slidesPerView: 1.5,
+	spaceBetween: 30,
+	loop: true,
+	breakpoints: {
+		767: {
+			slidesPerView: 2
+		},
+		991: {
+			slidesPerView: 3
+		}
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true
+	}
 });
