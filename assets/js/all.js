132bfdfd89d7470c238af49bebfed1e476c08e1e
wendy03 <wendysu03@gmail.com>
2020-12-21 22:27:31 +0800

    Update 2020-12-21T14:25:50.282Z

---
+++ assets/js/all.js
@@ -0,0 +1,47 @@
+"use strict";
+
+$(document).ready(function () {
+  $("#dropdownDate").daterangepicker();
+});
+var swiper = new Swiper(".swiper-top", {
+  slidesPerView: 1.3,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2
+    },
+    991: {
+      slidesPerView: 3
+    },
+    1200: {
+      slidesPerView: 4
+    }
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+var swiper = new Swiper(".swiper-inspiration", {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2
+    },
+    991: {
+      slidesPerView: 3
+    }
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+//# sourceMappingURL=all.js.map
